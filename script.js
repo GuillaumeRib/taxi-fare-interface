8a7009dc4c3176ec909dfca2e6f30c67bd8bcd84
Guillaume Ribiere <gribiere@gmail.com>
2022-09-02 13:06:32 +0200

    changing to fare_amount

--- script.js
+++ script.js
@@ -208,9 +208,9 @@ const predict = () => {
       })
       .then(response => response.json())
       .then(data => {
-        document.getElementById('fare_amount').classList.remove('d-none');
-        const fareResult = document.getElementById('predicted-fare');
-        const fare = Math.round(data['fare_amount'] * 100) / 100
+        document.getElementById('fare').classList.remove('d-none');
+        const fareResult = document.getElementById('fare_amount');
+        const fare = Math.round(data['fare'] * 100) / 100
         fareResult.innerText = `$${fare}`;
       })
       .catch((error) => {
